4a8f5c25b7be878bc63600ae6fc54c76fcdd4824
Chrissy Stroud <Chrissy.Stroud@ou.edu>
2019-02-25 11:52:12 -0600

    GotJSON

---
+++ GotJSON/GotJSON/HerokuQuote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GotJSON
+{
+    class HerokuQuote
+    {
+        public string character { get; set;}
+
+        public string quote { get; set; }
+
+        TextBlock txt = new TextBlock();
+        txt.FontSize = 32; // 24 points
+            txt.Inlines.Add("https://got-quotes.herokuapp.com/quotes");
+    }
+}
