016cf2bc45416fa5ee2920e79c5ce96d81b410f4
Chrissy Stroud <Chrissy.Stroud@ou.edu>
2019-02-27 11:42:01 -0600

    Started Participation Classes

---
+++ ClassesParticipation/ClassesParticipation/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesParticipation
+{
+    class Course
+    {
+        public int CourseNumber { get; set; }
+        public string CourseName { get; set; }
+        public string Subject { get; set; }
+        public string Instructor { get; set; }
+
+        public Course()
+        {
+
+        }
+
+        public ToString()
+        {
+            string;
+        }
+    }
+}
---
+++ ClassesParticipation/ClassesParticipation/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesParticipation
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int StudentID { get; set; }
+        public List<Course> Courses { get; }
+
+        public Student()
+        {
+            //fill out normal default values
+            Courses = new List<Course>(); //Create our new list
+        }
+
+            
+    }
+}
